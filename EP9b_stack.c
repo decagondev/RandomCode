8a95e1d050acf56963ebcfbc6fd327a181267058
Decagon <decagondevelopment@gmail.com>
2014-06-11 13:32:31 +0100

    Create EP9b_stack.c
    This is the follow up from the previous part in my 2 part video of creating a generic stack in C

---
+++ EP9b_stack.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include "stack.h"
+
+void sNew(stack *s, int elemSize, void (*freefn)(void *))
+{
+	assert(s->elemSize > 0);
+	s->elemSize = elemSize;
+	s->logicalLength = 0;
+	s->alocatedLength = 8;
+	s->elems = malloc(8 * elemSize);
+	assert(s->elems != NULL);
+}
+
+
+void sDispose(stack *s)
+{
+	if (s->freefn != NULL)
+	{
+		for(int i = 0; i < s->logicalLength; i++)
+		{
+			s->freefn((char *)s->elems + i * s->elemSize);
+		}
+	}
+	free(s->elems);
+}
+
+
+void sPush(stack *s, void *elemAddress)
+{
+	if(s->logicalLength == s->allocatedLength)
+		sGrow(s);
+	void *target = (char*)s->elems + s->logicalLength * s->elemSize;
+	memcpy(target, elemAddress, s->elemSize);
+	s->logicalLength++;
+}
+
+
+void sPop(stack *s, void *elemAddress)
+{
+	s->logicalLength--;
+	void *sorce = (char*)s->elems + s->logicalLength * s->elemSize;
+	memcpy(elemAddress, source, s->elemSize);
+}
+
+
+static void sGrow(stack *s)
+{
+	s->allocatedLength *= 2;
+	s->elems = realloc(s->elems, s->alocatedLength * s->elemSize);
+}
